0ce37a883fc402c5b62698ebdaf77f0940f81e79
afuku08 <fukkuseiya@gmail.com>
2025-06-09 11:30:48 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+    const todos = $state([
+        { id: 1, title: "Hello, Svelte"},
+        { id: 2, title: "Svelte is awesome"},
+    ]);
+
+    let uid = todos.length + 1;
+</script>
+
+<h1>home</h1>
+
+<div>
+    <input
+        placeholder="todo"
+        onkeydown={(e) => {
+            if (e.key !== 'Enter') return;
+
+            todos.push({
+                id: uid++,
+                title: e.currentTarget.value
+            });
+
+            e.currentTarget.value = '';
+        }}
+    />
+</div>
+
+{#snippet showTodo(post)}
+    <article>
+        <h2>{post.title}</h2>
+    </article>
+{/snippet}
+
+{#each todos as todo}
+    {@render showTodo(todo)}
+{/each}
+
